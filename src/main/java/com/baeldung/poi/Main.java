bbeea6fecad45f8027c31c4e3952c0011344d834
MonsieurVictor <34025717+MonsieurVictor@users.noreply.github.com>
2019-12-02 15:44:32 +0300

    added docs
    added docs

--- src/main/java/com/baeldung/poi/Main.java
+++ src/main/java/com/baeldung/poi/Main.java
@@ -23,8 +23,32 @@ public class Main {
     public static String output = "rest-with-spring.docx";
 
     public static void main(String args[]) throws Exception {
-        WordModifierNoIntact wordModifierNoIntact = new WordModifierNoIntact();
-        wordModifierNoIntact.start();
+//        WordModifierNoIntact wordModifierNoIntact = new WordModifierNoIntact();
+//        wordModifierNoIntact.start();
+
+//        WordKeepFormat wordKeepFormat = new WordKeepFormat();
+//        Path msWordPath = Paths.get("C://disV5.docx");
+//        XWPFDocument document2 = new XWPFDocument(Files.newInputStream(msWordPath));
+//        List<XWPFParagraph> paragraphs = document2.getParagraphs();
+//        document2.close();
+//        System.out.println("_________________________________________");
+//        for (XWPFParagraph paragraph : paragraphs) {
+//            wordKeepFormat.replace(paragraph, "студент", "студент!!!!!!!!!");
+//        };
+
+
+        XWPFDocument document = new XWPFDocument(new FileInputStream("C://styles3.docx"));
+
+        System.out.println(document.getStyles().styleExist("SAMOSVAT"));
+        System.out.println(document.getStyles().getStyle("SAMOSVAT").getName());
+
+        XWPFParagraph paragraph=document.createParagraph();
+        paragraph.setStyle("SAMOSVAT");
+        XWPFRun run=paragraph.createRun();
+        run.setText("TEST");
+
+        document.write(new FileOutputStream("docs//stylestest.docx"));
+        document.close();
     }
 }
 
